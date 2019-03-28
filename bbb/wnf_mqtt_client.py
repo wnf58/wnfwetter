3ca2be03db4667329700b7dd2ba7975d9f3eacfd
wnf58 <dev@wlsoft.de>
2019-03-28 06:48:44 +0100

    Es geht los 3

--- bbb/wnf_mqtt_client.py
+++ bbb/wnf_mqtt_client.py
@@ -3,22 +3,49 @@
 # Test
 
 import paho.mqtt.client as mqtt
+import time
+import json
+import geheim
 
-BONE2013 = '192.168.80.106'
+aLetzteTemp = 0.0
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-
     client.subscribe("computer/esp1")
 
+
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+    global aLetzteTemp
+    try:
+        # print(client)
+        # print(userdata)
+        # print(msg)
+        # print(msg.topic + " " + str(msg.payload))
+        aJson = json.loads(msg.payload.decode('utf8'))
+        # print(aJson)
+        # print(aJson['TEMP'])
+        x = aJson['TEMP']
+        x = float("{0:.1f}".format(x))
+        if ((x >= aLetzteTemp + 0.25) or (x <= aLetzteTemp - 0.25)):
+            aLetzteTemp = x
+            # print(aJson['TIME'])
+            # t = aJson['TIME']
+            # s = time.strftime("%b %d %Y %H:%M:%S", time.gmtime(t))
+            s = time.strftime("%H:%M:%S")
+            print('Messung: %s  Temperatur %s°C' % (s, "{0:.1f}".format(x)))
+    except Exception as E:
+        print('Fehler', E)
+        raise
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
 
-client.connect(BONE2013, 1883, 60)
+def main():
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(geheim.BBB_IP, 1883, 60)
+    client.loop_forever()
 
-client.loop_forever()
 
+if __name__ == '__main__':
+    main()
